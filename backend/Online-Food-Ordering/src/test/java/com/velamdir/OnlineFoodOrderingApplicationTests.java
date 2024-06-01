400d9438eda4719cfe6d7074a7c14fb81b2b99e5
Prakhar Tripathi <110407222+gurawliprakhar@users.noreply.github.com>
2024-06-01 13:44:00 +0530

    Initial commit

---
+++ backend/Online-Food-Ordering/src/test/java/com/velamdir/OnlineFoodOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velamdir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
