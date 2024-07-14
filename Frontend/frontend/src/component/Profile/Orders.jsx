1ca2c277b901279a790141a66df520291a7dfc96
Prakhar Tripathi <110407222+gurawliprakhar@users.noreply.github.com>
2024-07-14 15:15:48 +0530

    completed Cart ... now writing profile

---
+++ Frontend/frontend/src/component/Profile/Orders.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Orders = () =>  {
+  return (
+    <div className='flex items-center flex-col'>
+
+    </div>
+  )
+}
---
+++ Frontend/frontend/src/component/Profile/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import UserProfile from "./UserProfile";
+import Address from "./Address";
+import Favorites from "./Favorites";
+import Event from "./Event";
+import { ProfileNavigation } from "./ProfileNavigation";
+import { Orders } from "./Orders";
+
+const Profile = () => {
+  return (
+    <div className="lg:flex justify-between">
+      <div className="sticky h-[80vh] lg:w-[20%]">
+        <ProfileNavigation />
+      </div>
+      <div className="lg:w-[80%]">
+        <Routes>
+          <Route path="/" element={<UserProfile />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="address" element={<Address />} />
+          <Route path="favorites" element={<Favorites />} />
+          <Route path="events" element={<Event />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
