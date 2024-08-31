39b3f4396ab2e2f3e6c0f42b3c78f14cdb63c830
Prakhar Tripathi <110407222+gurawliprakhar@users.noreply.github.com>
2024-08-31 13:59:23 +0530

    working

--- Frontend/frontend/src/component/Auth/RegisterForm.jsx
+++ Frontend/frontend/src/component/Auth/RegisterForm.jsx
@@ -1,30 +1,30 @@
-import { Button, TextField, Typography } from "@mui/material";
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material";
 import { Field, Form, Formik } from "formik";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const intitalValues = {
-  fullname:"",
+  fullname: "",
   email: "",
   password: "",
-  role:"ROLE_CUSTOMER"
+  role: "",
 };
 
 function RegisterForm() {
-  const navigate=useNavigate();
-  const handleSubmit=()=>{}
+  const navigate = useNavigate();
+  const handleSubmit = () => {};
 
   return (
     <div>
       <Typography variant="h5" className="text-center">
-        Login
+        Register
       </Typography>
       <Formik onSubmit={handleSubmit} intitalValues={intitalValues}>
         <Form>
           <Field
             as={TextField}
-            name="email"
-            label="email"
+            name="fullName"
+            label="full name"
             fullWidth
             variant="outlined"
             margin="normal"
@@ -37,6 +37,20 @@ function RegisterForm() {
             variant="outlined"
             margin="normal"
           />
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Role</InputLabel>
+            <Field
+            as={Select}
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              //value={age}
+              label="Age"
+             // onChange={handleChange}
+            >
+              <MenuItem value={"ROLE_CUSTOMER"}>Customer</MenuItem>
+              <MenuItem value={"ROLE_RESTAURANT_OWNER"}>Restaurant Owner</MenuItem>
+            </Field>
+          </FormControl>
           <Button
             sx={{ mt: 2, padding: "1" }}
             fullWidth
@@ -46,8 +60,8 @@ function RegisterForm() {
         </Form>
       </Formik>
       <Typography variant="body2" align="center" sx={{ mt: 3 }}>
-        Don't have an account?
-        <Button size="small" onCLick={() => navigate("account/register")}>
+        if have an already account?
+        <Button size="small" onCLick={() => navigate("account/login")}>
           register
         </Button>
       </Typography>
